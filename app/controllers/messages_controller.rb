6a374610f7d2f89efa97f9be94021d48d0174a2a
Max Trubetskoy <handcrab@gmail.com>
2015-02-23 17:20:56 +0300

    allow to poke the cow from the index page

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,13 @@
 class MessagesController < ApplicationController
   def index
-    @messages = Message.all
+    @messages = Message.all.order created_at: :desc
+  end
+
+  def poke_cow
+    if Message.create body: Message.poke_cow
+      redirect_to root_path
+    else
+      redirect_to root_path, alert: 'The cow is broken'
+    end
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ActiveRecord::Base
-  def poke_cow
-    cow_msg = `sleep 2 && fortune | cowsay`
-    self.update body: cow_msg
+  def self.poke_cow
+    #self.update body: cow_msg
+    `sleep 3 && fortune | cowsay`
   end
 end
